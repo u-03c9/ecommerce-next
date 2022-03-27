feb71869256723bd67a0d21dbdcececf3e3923e6
Peter Khalil <uni.03c9@gmail.com>
2022-03-27 15:05:21 +0200

    new: re-exporting modules

--- framework/shopify/products/get-all-products.ts
+++ framework/shopify/products/get-all-products.ts
@@ -1,7 +1,9 @@
-import fetchApi from "../utils/fetch-api";
-import getAllProductsQuery from "../utils/queries/get-all-products";
+import {
+  fetchApi,
+  normalizeProduct,
+  getAllProductsQuery,
+} from "../utils/index";
 import { ProductConnection } from "../schema";
-import { normalizeProduct } from "../utils/normalize";
 import { Product } from "@common/types/product";
 
 type ReturnType = {
